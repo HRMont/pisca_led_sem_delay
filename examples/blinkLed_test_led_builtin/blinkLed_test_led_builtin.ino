d2dd6d629a55ef6073adf880e9ad5375c9d5e991
HRMont <143529486+HRMont@users.noreply.github.com>
2024-03-03 20:31:38 -0300

    Add files via upload

---
+++ examples/blinkLed_test_led_builtin/blinkLed_test_led_builtin.ino
@@ -0,0 +1,27 @@
+/*
+---------------------------------------------------------------------------------------------------
+Este exemplo demonstra como utilizar a biblioteca testando o led da placa Arduino
+Autor: Hercules Montenegro @ mar-2024
+
+link: https://github.com/HRMont/piscar-led-sem-delay.git
+---------------------------------------------------------------------------------------------------
+
+Requer a biblioteca: timerwithoutdelay: https://github.com/casaautomacao/timerwithoutdelay.git
+
+*/
+
+#include <BlinkLed.h>
+
+BlinkLed led(LED_BUILTIN);    // Led | Padrões: tempo LIGADO = 1000, tempo DESLIGADO = 1000
+                     
+void setup() {
+
+  // Inicializa os temporizadores, define o pino como OUTPUT e LOW (DESLIGADO)
+  led.begin();
+}
+
+void loop() {
+
+  // Chamada da função principal que inicia o ciclo de piscar
+  led.blink();
+}
