7afdc1efa63dfb5fd525aa12911e9ace2ad10103
HRMont <143529486+HRMont@users.noreply.github.com>
2024-03-03 20:26:26 -0300

    Update and rename blinkLed_test_led_internal.ino to blinkLed_test_led_builtin.ino

--- examples/blinkLed_test_led_internal/blinkLed_test_led_builtin.ino
+++ examples/blinkLed_test_led_internal/blinkLed_test_led_builtin.ino
@@ -1,6 +1,6 @@
 /*
 ---------------------------------------------------------------------------------------------------
-Este exemplo demonstra como utilizar a biblioteca testando o led interno do Arduino
+Este exemplo demonstra como utilizar a biblioteca testando o led da placa Arduino
 Autor: Hercules Montenegro @ mar-2024
 
 link: https://github.com/HRMont/piscar-led-sem-delay.git
@@ -12,9 +12,8 @@ Requer a biblioteca: timerwithoutdelay: https://github.com/casaautomacao/timerwi
 
 #include <BlinkLed.h>
 
-BlinkLed led(13);    // Pino padrão = 13 | Padrões: tempo LIGADO = 1000, tempo DESLIGADO = 1000
-                     // ATENÇÃO PARA CONFLITOS COM O PINO 13
-
+BlinkLed led(LED_BUILTIN);    // Led | Padrões: tempo LIGADO = 1000, tempo DESLIGADO = 1000
+                     
 void setup() {
 
   // Inicializa os temporizadores, define o pino como OUTPUT e LOW (DESLIGADO)
